5212c2de28df1d26e9d766566fc706efe7c3da1e
njchristian <njchristian@neo.tamu.edu>
2014-11-17 00:47:15 -0600

    Export csv file

--- popUp/app/assets/javascripts/reportSetup.js
+++ popUp/app/assets/javascripts/reportSetup.js
@@ -35,5 +35,35 @@ $(function(){
         }
     
     });
+    
+    $('#download').click( function(e){
 
+        var csv = "data:text/csv;charset=utf-8,\n";
+        
+        $('tr').each( function(){
+        
+            $('td', this).each( function(){
+            
+                csv = csv + this.textContent + ',';
+            
+            });
+            //remove latent comma
+            csv = csv.substring(0, csv.length - 1);
+        
+            csv = csv + '\n';
+        
+        });
+    
+        //console.log(csv);
+        
+        //http://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
+        var encodedUri = encodeURI(csv);
+        var link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "my_data.csv");
+
+        link.click();
+    
+    });
+    
 });
