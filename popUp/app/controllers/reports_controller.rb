9acb90e5994889c931e57c1905dcf105cdad35e1
njchristian <njchristian@neo.tamu.edu>
2014-11-16 18:02:38 -0600

    Update

--- popUp/app/controllers/reports_controller.rb
+++ popUp/app/controllers/reports_controller.rb
@@ -2,4 +2,8 @@ class ReportsController < ApplicationController
     def index
         @movies = 1
     end
+    
+    def show
+        @query = params[:id]
+    end
 end
--- popUp/config/routes.rb
+++ popUp/config/routes.rb
@@ -1,5 +1,8 @@
 PopUp::Application.routes.draw do
-    resources :reports
+
+    constraints(id: /[A-Za-z0-9-\*\%]+/ ) do
+        resources :reports
+    end
     resources :calendars
     
 root :to => redirect('calendars')
