bea6c1c7716e3b60a5fe75c17894b872d5e0ab71
caharsha <harshacopparam@gmail.com>
2014-12-05 23:04:24 -0600

    Updated server side UI with login authentication messages.

--- popUp/app/controllers/admins_controller.rb
+++ popUp/app/controllers/admins_controller.rb
@@ -16,9 +16,11 @@ class AdminsController < ApplicationController
         if username == "uname" and password == "pword"
             session[:loggedIn] = true
             redirect_to reports_path
+            flash[:success] = "Logged in successfully!"
         else
             session[:loggedIn] = false
             redirect_to admins_path
+            flash[:success] = "Invalid username/password combination."
         end
         
         
