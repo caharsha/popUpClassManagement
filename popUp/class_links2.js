92d96dabe63e1c0f2c9d7def159093f5e22fc0b6
caharsha <caharsha@neo.tamu.edu>
2014-12-09 12:24:27 -0600

    Create class_links2.js

---
+++ popUp/class_links2.js
@@ -0,0 +1,86 @@
+var eventList =  [
+	{
+		start: "2014-12-01T04:00:00",
+		id: "201",
+		title: "Safety",
+		url: "https://www.facebook.com",
+		description: "safety"
+    },
+	{
+		start: "2014-12-01T04:00:00",
+		id: "301",
+		title: "Mills",
+		url: "https://www.google.com/",
+		description: "Mills"
+	},
+	{
+		start: "2014-12-07T13:00:00",
+		id: "401",
+		title: "Android",
+		url: "https://www.yahoo.com/",
+		description: "Android"
+	},
+	{
+		start: "2014-12-17T07:00:00",
+		id: "501",
+		title: "Welding",
+		url: "http://www.msn.com/",
+		description: "Welding"
+	},
+	{
+		start: "2014-12-08T05:00:00",
+		id: "601",
+		title: "Effective Communication",
+		url: "https://www.facebook.com",
+		description: "Effective Communication"
+	},
+	{
+		start: "2014-12-09T05:00:00",
+		id: "701",
+		title: "Group Projects",
+		url: "https://www.google.com/",
+		description: "Group Projects"
+	},
+	{
+		start: "2014-12-23T15:00:00",
+		id: "801",
+		title: "Shopbot Basics",
+		url: "https://www.yahoo.com/",
+		description: "Shopbot Basics"
+	},
+	{
+		start: "2014-12-18T09:00:00",
+		id: "901",
+		title: "Shopbot Programming",
+		url: "http://www.msn.com/",
+		description: "Shopbot Programming"
+	},
+	{
+		start: "2014-12-12T06:00:00",
+		id: "1001",
+		title: "Laser Cutter",
+		url: "https://www.facebook.com",
+		description: "Lasers! Woah!"
+	},
+	{
+		start: "2014-12-14T06:00:00",
+		id: "1101",
+		title: "3D Printing",
+		url: "https://www.eventbrite.com/e/eic-pop-up-class-3d-printing-use-and-job-submission-tickets-12166691903",
+		description: "3D Printing and Submission"
+	},
+	{
+		start: "2014-12-04T06:00:00",
+		id: "1201",
+		title: "Basic Lathe Handling",
+		url: "https://www.yahoo.com/",
+		description: "Basic Lathe Handling"
+	},
+	{
+		start: "2014-12-11T10:00:00",
+		id: "1301",
+		title: "Advanced Lathe Operation",
+		url: "http://www.msn.com/",
+		description: "Advanced Lathe Operation"
+	}
+];
